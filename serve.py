94325bd012d4849e3fbb0592daf142f43b02c3a5
jinh9015 <125936456+jinh9015@users.noreply.github.com>
2023-06-16 19:21:02 +0900

    Update serve.py

--- serve.py
+++ serve.py
@@ -22,6 +22,10 @@ def predict():
 
     return jsonify({'output': result.tolist()})
 
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"status": "ok"})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
 
